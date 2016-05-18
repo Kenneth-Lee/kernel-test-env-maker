cbdafba2563f950992558032790a0547435bd91c
Kenneth Lee <kenneth-lee-2012@qq.com>
2016-05-18 18:38:07 +0800

    update for newer kernel and qemu version
    1. config for 4.6 and enable virtio
    2. enable kvm in run

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-/usr/bin/qemu-system-x86_64 -nographic \
+#if use drive which if=ide, set root to /dev/sda1
+#if use drive which if=virtio, set root to /dev/vda1
+
+/usr/bin/qemu-system-x86_64 -nographic -drive file=rootfs.img,format=raw,index=0,if=virtio \
+	-enable-kvm \
 	-kernel arch/x86_64/boot/bzImage \
-	-m 512 \
-	-net nic,model=ne2k_pci -net user,tftp=. \
-	-drive file=rootfs.img,index=0,media=disk \
-	-append "rw console=ttyS0 root=/dev/sda1"
+	-append "rw console=ttyS0 root=/dev/vda1 init=/bin/bash"
